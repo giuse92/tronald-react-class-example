600023ec9788f22a8398324a1050f8f85d050182
Giuseppe <giuseppe.ilsami@gmail.com>
2020-11-07 22:13:14 +0100

    piccoli accorgimenti

--- src/components/CurrentQuote.js
+++ src/components/CurrentQuote.js
@@ -6,7 +6,7 @@ const CurrentQuote = ({ currentQuoteState, isListMode, selectedTag, storedQuotes
     const day = d.getDate()
     const month = monthsTxt[d.getMonth()];
     const year = d.getFullYear();
-    const filtedQuotes = storedQuotes.filter(obj => selectedTag === obj.tags[0]);
+    const filteredQuotes = storedQuotes.filter(obj => selectedTag === obj.tags[0]);
 
     return (
         <>  
@@ -15,7 +15,7 @@ const CurrentQuote = ({ currentQuoteState, isListMode, selectedTag, storedQuotes
                     {selectedTag !== '' ?
                         <div style={{width: '50%', fontSize: '22px'}}>
                             <ul style={{listStyle: 'none'}}>
-                                {filtedQuotes.map((q, i) => {
+                                {filteredQuotes.map((q, i) => {
                                     return (
                                     <li key={`filted-quotes-${i}`} style={{borderBottom: '2px solid white'}}>
                                         {q.value}
@@ -34,7 +34,7 @@ const CurrentQuote = ({ currentQuoteState, isListMode, selectedTag, storedQuotes
                     <q style={{ color: 'red' }}>{currentQuoteState.value}</q>
                     <div>
                         <span>&#127919; </span>
-                        <i>{currentQuoteState.tags.length === 0 ? 'No target detected' : currentQuoteState.tags}</i>
+                        <i>{currentQuoteState.tags.length === 0 ? 'No target detected' /* Empty currentQuoteState.tags */ : currentQuoteState.tags}</i>
                     </div>
                     <div>
                         <span>&#128198; </span>
