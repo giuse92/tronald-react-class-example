7068a8d29e0e1338da2b455f9ab0f56eb9360204
Giuseppe <giuseppe.ilsami@gmail.com>
2020-11-16 12:39:29 +0100

    hooks prima di removeQuoteFromList e prima del return

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './trump.gif';
 import TagList from './components/TagList';
@@ -29,11 +29,26 @@ const RANDOMURL = 'https://api.tronalddump.io/random/quote'
 // const ALLTAGSURL = 'https://api.tronalddump.io/tag'
 const bidenSmilingSrc = 'https://d3g9pb5nvr3u7.cloudfront.net/authors/59bb2de59744155625a7c141/-435182605/256.jpg'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
+const App = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [currentQuote, setCurrentQuote] = useState(((JSON.parse(localStorage.getItem('trumpCurrentQuote'))) !== null
+    ? (JSON.parse(localStorage.getItem('trumpCurrentQuote')))
+    : {}));
+  const [quotesToShow, setQuotesToShow] = useState([]);
+  const [storedQuotes, setStoredQuotes] = useState(((JSON.parse(localStorage.getItem('trumpQuotes'))) !== null
+    ? (JSON.parse(localStorage.getItem('trumpQuotes')))
+    : []));
+  const [storedTags, setStoredTags] = useState(((JSON.parse(localStorage.getItem('trumpQuotesTags'))) !== null
+    ? (JSON.parse(localStorage.getItem('trumpQuotesTags'))) 
+    : []));
+  const [selectedTag, setSelectedTag] = useState('');
+  const [isListMode, setIsListMode] = useState(false);
+  const [btnSaveDisabled, setBtnSaveDisabled] = useState(JSON.parse(localStorage.getItem('trumpQuotesTags')) === null
+    ? true 
+    : false);
+
+      /*loading: false,
       error: false,
       currentQuote: ((JSON.parse(localStorage.getItem('trumpCurrentQuote'))) !== null
         ? (JSON.parse(localStorage.getItem('trumpCurrentQuote')))
@@ -48,17 +63,16 @@ class App extends React.Component {
       selectedTag: '',
       isListMode: false,
       btnSaveDisabled: JSON.parse(localStorage.getItem('trumpQuotesTags')) === null 
-      ? true : false
-    }
-  }
+      ? true : false*/
 
   // dividere in fetchRandomTrump() e saveRandomTrump()
-  fetchRandomTrump = async () => {
+  const fetchRandomTrump = async () => {
     let quote = {}
     let error = false
 
     try {
-      this.setState({ loading: true })
+      setLoading(true);
+      //this.setState({ loading: true })
       let response = await fetch(RANDOMURL)
       let data = await response.json()
       //console.log('NEL TRY DATA: ', data)
@@ -76,7 +90,13 @@ class App extends React.Component {
     } finally {
       // using setState with prevState
       // see https://css-tricks.com/understanding-react-setstate/
-      this.setState((prevState) => {
+      localStorage.setItem('trumpCurrentQuote', JSON.stringify(quote));
+      setCurrentQuote(error ? {} : quote);
+      setLoading(false);
+      setError(error);
+      setBtnSaveDisabled(false);
+            
+      /*this.setState((prevState) => {
         localStorage.setItem('trumpCurrentQuote', JSON.stringify(quote))
         return {
           ...this.state, // see immutables
@@ -85,18 +105,32 @@ class App extends React.Component {
           error,
           btnSaveDisabled: false
         }
-      })
+      })*/
     }
   }
 
-  saveRandomQuote = (event) => {
-    const storedQuotes = this.state.storedQuotes
-    let storedTags = this.state.storedTags
+  const saveRandomQuote = (event) => {
+    const storedQuotesX = storedQuotes;
+    //const storedQuotes = this.state.storedQuotes
+    let storedTagsX = storedTags;
+    //let storedTags = this.state.storedTags
     // see inside quote.tags.forEach
     // let storedTags = [...this.state.storedTags]
     let isNewQuote = true;
 
     // checking for each retrieved tag, if exists
+    if (currentQuote.tags.length > 0) {
+      currentQuote.tags.forEach(currentTag => {
+        const indexTag = storedTagsX.findIndex(storedTag => storedTag === currentTag)
+        if (indexTag === -1) {
+          // mutable operation will lead to bugs here
+          // storedTags.push(currentTag)
+          storedTagsX = [...storedTagsX, currentTag]
+        }
+      })
+    }
+
+    /*
     if (this.state.currentQuote.tags.length > 0) {
       this.state.currentQuote.tags.forEach(currentTag => {
         const indexTag = storedTags.findIndex(storedTag => storedTag === currentTag)
@@ -107,9 +141,19 @@ class App extends React.Component {
         }
       })
     }
+    */
 
     // checking stored quotes
     // avoid condition if array is empty
+    if (storedQuotesX.length > 0) {
+      // check if quote already exists
+      const indexQuote = storedQuotesX.findIndex(storedQuote => currentQuote.quote_id === storedQuote.quote_id)
+      if (indexQuote > -1) { // this means that quote already exists!
+        isNewQuote = false
+      }
+    }
+
+    /* 
     if (storedQuotes.length > 0) {
       // check if quote already exists
       const indexQuote = storedQuotes.findIndex(storedQuote => this.state.currentQuote.quote_id === storedQuote.quote_id)
@@ -117,8 +161,16 @@ class App extends React.Component {
         isNewQuote = false
       }
     }
+    */
 
-    this.setState((prevState) => {
+
+    const quotesToSave = (isNewQuote && error !== true) ? [...storedQuotesX, currentQuote]: storedQuotes;
+    localStorage.setItem('trumpQuotes', JSON.stringify(quotesToSave));
+    localStorage.setItem('trumpQuotesTags', JSON.stringify(storedTags));
+    setStoredQuotes([...quotesToSave]);
+    setStoredTags([...storedTagsX]);
+    setBtnSaveDisabled(true);
+    /*this.setState((prevState) => {
       const quotesToSave = (isNewQuote && this.state.error !== true) ? [...prevState.storedQuotes, this.state.currentQuote] : prevState.storedQuotes
       // storing into localStorage
       localStorage.setItem('trumpQuotes', JSON.stringify(quotesToSave))
@@ -129,10 +181,10 @@ class App extends React.Component {
         storedTags: [...storedTags],
         btnSaveDisabled: true
       }
-    })
+    })*/
   }
 
-  removeQuoteFromList = (quoteIdList) => {
+  const removeQuoteFromList = (quoteIdList) => {
     this.setState((prevState) => {
       const indexQ = this.state.storedQuotes.findIndex(obj => obj.quote_id === quoteIdList);
       const newStoredQ = [...this.state.storedQuotes.slice(0, indexQ), ...this.state.storedQuotes.slice(indexQ + 1)];
@@ -151,9 +203,9 @@ class App extends React.Component {
     })
   }
 
-  onTagClick = (event) => this.setState({ selectedTag: event.target.name })
+  const onTagClick = (event) => this.setState({ selectedTag: event.target.name })
   
-  onModeClick = (mode) => (event) => {
+  const onModeClick = (mode) => (event) => {
     // console.log('MODE? ', mode)
     this.setState({ 
       isListMode: event.currentTarget.id === 'listbutton' ? true : false,
@@ -161,12 +213,11 @@ class App extends React.Component {
     })
   } 
 
-  componentDidUpdate(prevProps, prevState) {
+  /*componentDidUpdate(prevProps, prevState) {
     // console.log('PROBLEM!!! ', prevState.storedTags.length, this.state.storedTags.length)
     if (prevState.storedTags.length !== this.state.storedTags.length) console.log('DIFFERENZA STOREDTAG!')
-  }
+  }*/
 
-  render () {
     return (
       <div className="App">
         <header className="App-header">
@@ -209,7 +260,6 @@ class App extends React.Component {
         </header>
       </div>
     );
-  }
 }
 
 export default App;
